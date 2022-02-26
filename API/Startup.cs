1eed5a9ceff268721b7cf87124fccaf3cbbae570
eng.ahmedbalousha@outlook.com <eng.ahmedbalousha@outlook.com>
2022-02-26 19:44:22 +0200

    End of secion 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,12 +38,18 @@ namespace API
             services.AddDbContext<DataContext>(opt  => {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy",policy =>{
+                   policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+          });
          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -51,9 +57,10 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
